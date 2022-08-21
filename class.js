cb281e82b915ad0289bdd1eece700e9af4d3038e
swathi ravi <110903083+swathiravi-vh@users.noreply.github.com>
2022-08-21 16:41:09 +0530

    Add files via upload
    consider this

--- class.js
+++ class.js
@@ -1,3 +1,5 @@
+//write a class person hold all the details
+
 function Person() {
     this.fname ="";
     this.lname ="";
@@ -14,3 +16,19 @@ person2.fname = "Hemil";
 person2.lname = "vishnu";
 
 console.log(person2.fname +"  "+ person2.lname); 
+
+//write a class to calculate uber price.
+
+class UberPrice {
+    constructor(distance) {
+      this.distance = distance;
+      
+    }
+    getPrice() {
+      return (this.distance ) *20;
+    }
+  }
+  
+  let uber = new UberPrice(10);
+  console.log(uber.getPrice());
+
