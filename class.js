4a266217bb283790ccb25ea14d4aa5624b0be23a
swathi ravi <110903083+swathiravi-vh@users.noreply.github.com>
2022-08-21 16:26:31 +0530

    Add files via upload

---
+++ class.js
@@ -0,0 +1,16 @@
+function Person() {
+    this.fname ="";
+    this.lname ="";
+}
+
+let person1 = new Person();
+person1.fname = "swathi";
+person1.lname = "ravi";
+
+console.log(person1.fname+ "   " +  person1.lname);
+
+let person2 = new Person();
+person2.fname = "Hemil";
+person2.lname = "vishnu";
+
+console.log(person2.fname +"  "+ person2.lname); 
